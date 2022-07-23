6b516cb85a3fe6a44bec9d7c0575ad46c345aa21
Atm85 <stevecoulter99@gmail.com>
2022-07-22 19:47:25 -0600

    pass socketport to client

--- app/views/index.py
+++ app/views/index.py
@@ -1,7 +1,13 @@
+import os
+
 from app import server
 from flask import render_template
 
 
 @server.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    port = os.getenv("PORT")
+    if port is None:
+        port = 5000
+
+    return render_template("index.html", socket_port=port)
