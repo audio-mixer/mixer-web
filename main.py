a1042a9b3a537da2e41e48badb6a4955af81c00d
Atm85 <stevecoulter99@gmail.com>
2022-07-22 18:41:23 -0600

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
+
 from app import server
 
 
 def main():
-    server.run()
+    port = os.getenv("PORT")
+    if port is None:
+        port = 5000
+
+    server.run(host="0.0.0.0", port=port)
 
 
 if __name__ == '__main__':
