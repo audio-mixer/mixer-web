93622ed2560260c5ac77230d03175b38a0beb3f4
Atm85 <stevecoulter99@gmail.com>
2022-07-23 19:28:46 -0600

    implement websocket commands

--- app/sockets/audio_stream.py
+++ app/sockets/audio_stream.py
@@ -13,28 +13,38 @@ CHUNK_SIZE = 10 * 1024
 @socket.route("/stream")
 def audio_stream(ws: Websocket):
 
+    wav = None
+
     while ws.connected:
 
-        message = ws.receive()
-        data = json.loads(message)
+        message = ws.receive(timeout=0)
+        if message is not None:
+            data = json.loads(message)
+            if "source" in data:
+                wav = wave.open(data["source"])
+            if "command" in data:
+                if data["command"] == "STOP":
+                    wav.close()
+                    wav = None
+                    print("Stopped streaming...")
+                    continue
 
-        with wave.open(data["source"], 'rb') as wav:
+        if wav is not None:
+            if wav.tell() >= wav.getnframes():
+                wav.close()
+                wav = None
+                print("finished streaming data!")
+                continue
 
-            complete = False
             sample_rate = wav.getframerate()
-            while not complete:
-                if wav.tell() >= wav.getnframes():
-                    complete = True
+            frame = wav.readframes(CHUNK_SIZE)
+            data = b'RIFF' + struct.pack(
+                '<L4s4sLHHLLHH4s', 36 + wav.getnframes() * wav.getnchannels() * wav.getsampwidth(),
+                b'WAVE', b'fmt ', 16, 0x0001, wav.getnchannels(), wav.getframerate(),
+                wav.getnchannels() * wav.getframerate() * wav.getsampwidth(),
+                wav.getnchannels() * wav.getsampwidth(), wav.getsampwidth() * 8, b'data'
+            ) + struct.pack('<L', wav.getnframes() * wav.getnchannels() * wav.getsampwidth()) + frame
 
-                frame = wav.readframes(CHUNK_SIZE)
-                data = b'RIFF' + struct.pack(
-                    '<L4s4sLHHLLHH4s', 36 + wav.getnframes() * wav.getnchannels() * wav.getsampwidth(),
-                    b'WAVE', b'fmt ', 16, 0x0001, wav.getnchannels(), wav.getframerate(),
-                    wav.getnchannels() * wav.getframerate() * wav.getsampwidth(),
-                    wav.getnchannels() * wav.getsampwidth(), wav.getsampwidth() * 8, b'data'
-                ) + struct.pack('<L', wav.getnframes() * wav.getnchannels() * wav.getsampwidth()) + frame
+            ws.send(data)
+            time.sleep(0.8 * CHUNK_SIZE / sample_rate)
 
-                ws.send(data)
-                time.sleep(0.8 * CHUNK_SIZE / sample_rate)
-
-        print("finished streaming data!")
