89d8a54bf2f30155796e745a4da52a6b3fc22be9
Atm85 <stevecoulter99@gmail.com>
2022-07-25 20:01:26 -0600

    change public location

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask_sock import Sock
-server = Flask(__name__)
+
+server = Flask(
+    __name__,
+    static_url_path="/static",
+    static_folder="../web/static",
+    template_folder="../web/templates"
+)
+
 socket = Sock(server)
 
 # all other imports should be after this line, this is to prevent 'circular imports'
