5d569e62d28c483fd4b34468268e2bc786f69ae4
Atm85 <stevecoulter99@gmail.com>
2022-08-19 18:49:30 -0600

    refactoring...

--- app/sockets/audio_stream.py
+++ app/sockets/audio_stream.py
@@ -63,168 +63,216 @@ def audio_stream(ws: Websocket):
         ```
     """
 
-    wav = None
-    channels = None
-    _audio = None
-    _source = ""
-    _duration = {}
+    conn = Connection(ws)
+    while ws.connected:
+        conn.handle()
 
-    _queue = Queue()
 
-    # generator for YouTube streaming.
-    def get_chunk():
-        if _queue.not_empty:
-            chunk: AudioSegment = _queue.get()
+class Connection:
+
+    def dequeue_chunk(self):
+        """
+        gets the next chunk from the queue to be sent to the client
+        """
+        if self.queue.not_empty:
             buffer = io.BytesIO()
+            chunk: AudioSegment = self.queue.get()
             chunk.export(out_f=buffer, format="mp3")
             yield buffer
         else:
             raise StopIteration
 
-    # keep connection open.
-    while ws.connected:
+    def enqueue_chunk(self, chunk: AudioSegment):
+        """
+        enqueues the chunk when finished processing
+        """
+        self.queue.put(chunk)
+
+    def set_playback_speed(self, val: int):
+        """
+        changes the playback speed
+        """
+        if self.channels is not None:
+            value = val / 100
+            for chan in self.channels:
+                chan.filters["speed"].update(value)
+
+    def set_filter_intensity(self, val: int):
+        """
+        changes the 'lowpass' filter intensity
+        """
+        if self.channels is not None:
+            value = 0.5 - (math.log(val, 10) / 4)
+            for chan in self.channels:
+                chan.filters["lowpass"].kernel = filter.windowed_sinc_ir(value)
+
+    def get(self):
+        """
+        sends the duration of the song to the client
+        """
+        self.ws.send(json.dumps({
+            "command": "GET",
+            "duration": self.duration
+        }))
+
+    def next(self):
+        if self.source == "file":
+            if self.wav is None:
+                return
+
+            if self.wav.tell() >= self.wav.getnframes():
+                print("finished transmitting chunks!")
+                self.stop()
+                return
+
+            # read the next chunk and possess the channels
+            raw_wav = self.wav.readframes(BUFFER_SIZE)
+            self.channels = filter.process(self.channels, raw_wav, self.wav.getsampwidth())
+            processed_audio = filter.combine_wav_channels(self.channels, self.wav.getsampwidth())
+
+            # recreate the wav headers as the `wave` module strips them off :(
+            headers = b'RIFF' + struct.pack(
+                '<L4s4sLHHLLHH4s', 36 + self.wav.getnframes() * self.wav.getnchannels() * self.wav.getsampwidth(),
+                b'WAVE', b'fmt ', 16, 0x0001, self.wav.getnchannels(), self.wav.getframerate(),
+                self.wav.getnchannels() * self.wav.getframerate() * self.wav.getsampwidth(),
+                self.wav.getnchannels() * self.wav.getsampwidth(), self.wav.getsampwidth() * 8, b'data'
+            ) + struct.pack('<L', self.wav.getnframes() * self.wav.getnchannels() * self.wav.getsampwidth())
+
+            # finally, send the processed chunk to the client
+            self.ws.send(headers + processed_audio)
+        if self.source == "youtube":
+            if self.audio is None:
+                return
+
+            try:
+                self.ws.send(next(self.dequeue_chunk()).read())
+            except StopIteration:
+                _audio = None
+                print("finished transmitting chunks!")
+                return
+
+    def stop(self):
+        if self.source == "file":
+            self.wav.close()
+            self.wav = None
+            self.channels = None
+        if self.source == "youtube":
+            pass
+
+        self.query = ""
+        self.source = ""
+        self.audio = None
+        self.queue = Queue()
+        print("stopped transmitting...")
+
+    def process_stream(self):
+        """
+
+        """
+
+        # initially search YouTube
+        res = requests.get(f"{app.URL}/search?" + urllib.parse.urlencode({
+            "key": app.KEY,
+            "part": "id",
+            "maxResults": "1",
+            "type": "video",
+            "q": self.query
+        }), headers={"Accept": "application/json"})
+        videoid = res.json()["items"][0]["id"]["videoId"]
+
+        # get audio data from search result
+        file = io.BytesIO()
+        video = YouTube(f"https://www.youtube.com/watch?v={videoid}")
+        stream = video.streams.filter(only_audio=True, file_extension="mp4").first()
+        stream.stream_to_buffer(file)
+        file.seek(0)
+
+        # break data into a list of io.BytesIO objects
+        self.audio = AudioSegment.from_file(file)
+        [self.enqueue_chunk(chunk) for chunk in make_chunks(self.audio, chunk_length=1000)]
+
+        # calculate audio duration
+        self.duration = {
+            "hours": self.audio.duration_seconds // 3600,
+            "minuets": self.audio.duration_seconds // 60,
+            "seconds": math.floor(self.audio.duration_seconds % 60)
+        }
+
+    def handle(self):
+        """
+
+        """
 
         # don't block when timeout is set to 0
-        message = ws.receive(timeout=0)
-        if message is not None:
+        message = self.ws.receive(timeout=0)
+        if message is None:
+            return
 
-            data = json.loads(message)
+        # load parameters from websocket message
+        data = json.loads(message)
+        if "q" in data:
+            self.query = data["q"]
 
-            # read the `source` property sent from the client
-            # and open `wave` object for reading.
-            if "source" in data:
-                _source = data["source"]
-                if _source == "file":
-                    wav = wave.open(data["q"])
+        if "source" in data:
+            self.source = data["source"]
 
-                    # calculate the duration of the track, so it can be retrieved with the `GET` command.
-                    length = int(wav.getnframes() / wav.getframerate())
-                    hours = length // 3600
-                    length %= 3600
-                    minutes = length // 60
-                    length %= 60
-                    seconds = length
-                    _duration = {
-                        "hours": hours,
-                        "minuets": minutes,
-                        "seconds": seconds
-                    }
+        # stream local file from system
+        if self.source == "file":
+            if self.wav is None:
+                self.wav = wave.open(self.query)
+                self.channels = filter.create_channels(self.wav)
 
-                    # create `filter.Channel()'s` object for possessing filters.
-                    channels = filter.create_channels(wav)
+                # calculate the duration of the track, so it can be retrieved with the `GET` command.
+                length = int(self.wav.getnframes() / self.wav.getframerate())
+                hours = length // 3600
+                length %= 3600
+                minutes = length // 60
+                length %= 60
+                seconds = length
+                self.duration = {
+                    "hours": hours,
+                    "minuets": minutes,
+                    "seconds": seconds
+                }
 
-                if _source == "youtube":
-                    _query = data["q"]
+        # stream from YouTube source
+        if self.source == "youtube":
+            if self.audio is None:
+                start = time.perf_counter()
+                self.process_stream()
+                end = time.perf_counter()
+                print(f"Finished processing data in {round(end - start, 2)}")  # benchmarking...
 
-                    def process_stream():
-                        # initially search YouTube
-                        res = requests.get(f"{app.URL}/search?" + urllib.parse.urlencode({
-                            "key": app.KEY,
-                            "part": "id",
-                            "maxResults": "1",
-                            "type": "video",
-                            "q": _query
-                        }), headers={"Accept": "application/json"})
-                        videoid = res.json()["items"][0]["id"]["videoId"]
+        # handle commands
+        if "commands" in data:
+            for cmd in data["commands"]:
+                if cmd == "GET":
+                    self.get()
+                if cmd == "UPDATE_FILTER":
+                    value = int(data["value"])
+                    self.set_filter_intensity(value)
+                if cmd == "UPDATE_SPEED":
+                    value = int(data["value"])
+                    self.set_playback_speed(value)
+                if cmd == "NEXT":
+                    self.next()
+                if cmd == "STOP":
+                    self.stop()
 
-                        # get audio data from search result
-                        file = io.BytesIO()
-                        video = YouTube(f"https://www.youtube.com/watch?v={videoid}")
-                        stream = video.streams.filter(only_audio=True, file_extension="mp4").first()
-                        stream.stream_to_buffer(file)
-                        file.seek(0)
+    def __init__(self, ws: Websocket):
 
-                        # break data into a list of io.BytesIO objects
-                        audio = AudioSegment.from_file(file)
-                        [_queue.put(chunk) for chunk in make_chunks(audio, chunk_length=1000)]
+        # websocket connection
+        self.ws = ws
 
-                        # calculate audio duration
-                        duration = {
-                            "hours": audio.duration_seconds // 3600,
-                            "minuets": audio.duration_seconds // 60,
-                            "seconds": math.floor(audio.duration_seconds % 60)
-                        }
+        # queue for storing audio data chunks
+        self.queue = Queue()
 
-                        return audio, duration
+        # default values
+        self.query = ""
+        self.source = ""
 
-                    start = time.perf_counter()
-                    _audio, _duration = process_stream()
-                    end = time.perf_counter()
-                    print(f"Finished processing data in {round(end - start, 2)}")
-            else:
-                pass
-
-            # read the `commands` property sent from the client
-            if "commands" in data:
-                for command in data["commands"]:
-
-                    # handles the `GET` command
-                    if command == "GET":
-                        ws.send(json.dumps({
-                            "command": "GET",
-                            "duration": _duration
-                        }))
-
-                    # handles the `NEXT` command
-                    if command == "NEXT":
-                        if _source == "file":
-                            if wav is not None:
-                                if wav.tell() >= wav.getnframes():
-                                    wav.close()
-                                    wav = None
-                                    channels = None
-                                    print("finished transmitting chunks!")
-                                    continue
-
-                                # read the next chunk and possess the channels
-                                raw_wav = wav.readframes(BUFFER_SIZE)
-                                channels = filter.process(channels, raw_wav, wav.getsampwidth())
-                                processed_audio = filter.combine_wav_channels(channels, wav.getsampwidth())
-
-                                # recreate the wav headers as the `wave` module strips them off :(
-                                headers = b'RIFF' + struct.pack(
-                                    '<L4s4sLHHLLHH4s', 36 + wav.getnframes() * wav.getnchannels() * wav.getsampwidth(),
-                                    b'WAVE', b'fmt ', 16, 0x0001, wav.getnchannels(), wav.getframerate(),
-                                    wav.getnchannels() * wav.getframerate() * wav.getsampwidth(),
-                                    wav.getnchannels() * wav.getsampwidth(), wav.getsampwidth() * 8, b'data'
-                                ) + struct.pack('<L', wav.getnframes() * wav.getnchannels() * wav.getsampwidth())
-
-                                # finally, send the processed chunk to the client
-                                ws.send(headers + processed_audio)
-
-                        if _source == "youtube":
-                            if _audio is not None:
-                                try:
-                                    ws.send(next(get_chunk()).read())
-                                except StopIteration:
-                                    _audio = None
-                                    print("finished transmitting chunks!")
-                                    continue
-
-                    # handles the `UPDATE_FILTER` command
-                    if command == "UPDATE_FILTER":
-                        if channels is not None:
-                            value = int(data["value"])
-                            value = 0.5 - (math.log(value, 10) / 4)
-                            for chan in channels:
-                                chan.filters["lowpass"].kernel = filter.windowed_sinc_ir(value)
-
-                    # handles the `UPDATE_SPEED` command
-                    if command == "UPDATE_SPEED":
-                        if channels is not None:
-                            value = int(data["value"])
-                            value = value/100
-                            for chan in channels:
-                                chan.filters["speed"].update(value)
-
-                    # handles the `STOP` command
-                    if command == "STOP":
-                        if wav is not None:
-                            wav.close()
-                            wav = None
-                            channels = None
-                        if _audio is not None:
-                            _audio = None
-
-                        print("stopped transmitting...")
-
+        #
+        self.wav = None
+        self.audio = None
+        self.channels = None
+        self.duration = {"hours": 0, "minuets": 0, "seconds": 0}
